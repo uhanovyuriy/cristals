93749fded14c75abb2caf4ede6fbd85b5d2645fe
uhanov <uhanov@luxurygames.ch>
2021-05-26 17:26:28 +0200

    fist commit

---
+++ src/main/java/ch/boogaga/crystals/CrystalsApplication.java
@@ -0,0 +1,13 @@
+package ch.boogaga.crystals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrystalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrystalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/boogaga/crystals/CrystalsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.boogaga.crystals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrystalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
